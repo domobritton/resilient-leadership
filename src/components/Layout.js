f40602d50b7b20235f2b3c9850fa93ed78ff09db
Domonic Adams <domonicbritton@gmail.com>
2022-11-16 19:42:56 -0800

    gtag

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -44,11 +44,11 @@ const TemplateWrapper = ({
         {/* <!-- Google tag (gtag.js) --> */}
         <script
           async
-          src='https://www.googletagmanager.com/gtag/js?id=UA-114974500-1'
+          src='https://www.googletagmanager.com/gtag/js?id=G-1Q1CREHB00'
         ></script>
         <script
           dangerouslySetInnerHTML={{
-            __html: `window.dataLayer = window.dataLayer || []; function gtag(){dataLayer.push(arguments);} gtag('js', new Date()); gtag('config', 'UA-114974500-1');`,
+            __html: `window.dataLayer = window.dataLayer || []; function gtag(){dataLayer.push(arguments);} gtag('js', new Date()); gtag('config', 'G-1Q1CREHB00');`,
           }}
         />
         <link
